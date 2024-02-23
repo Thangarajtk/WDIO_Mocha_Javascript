ad55abcb3ccdeeab218c63b2d35b39810ad28438
Thangarajtk <thangarajatk@gmail.com>
2024-02-23 21:36:57 +0530

    Modified docker-compose to run test on MacOS silicon chip

--- src/conf/wdio.docker.conf.js
+++ src/conf/wdio.docker.conf.js
@@ -1,25 +1,24 @@
-import { config as sharedConfig } from './wdio.conf';
+import { config as sharedConfig } from './wdio.conf.js';
 
-exports.config = {
+export const config = {
     ...sharedConfig,
     ...{
         hostname: 'localhost',
         port: 4444,
         path: '/',
         maxInstances: 5,
-        services: ['docker'],
         capabilities: [{
             browserName: 'chrome',
             acceptInsecureCerts: true,
             'goog:chromeOptions': {
                 args: [
-                    '--no-sandbox',
-                    '--disable-infobars',
-                    '--disable-gpu',
-                    // '--headless',
-                    '--window-size=1440,735'
+                    '--disable-web-security', 
+                    '--disable-dev-shm-usage', 
+                    '--no-sandbox', 
+                    '--window-size=1920,1080'
                 ],
             }
         }],
+        services: ['docker']
     }
 }
